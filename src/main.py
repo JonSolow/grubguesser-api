0467e2d7a622488e2f704ceb2f066f62f446f095
JonSolow <jssolow+gh@gmail.com>
2022-04-02 17:40:00 -0400

    Add predict_file endpoint

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File
 from handler import handle_url
 from predict import predict_model
 
@@ -15,3 +15,8 @@ async def predict_url(url: str):
     model_input = handle_url(url)
     model_output = predict_model(model_input)
     return model_output
+
+
+@app.post("/predict_file")
+async def create_file(file: bytes = File(...)):
+    return {"file_size": len(file)}
