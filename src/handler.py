50eab01f4048794062be4c4b888fbe854bd73aa1
JonSolow <jssolow+gh@gmail.com>
2022-04-02 18:22:25 -0400

    Add predict_file endpoint

--- src/handler.py
+++ src/handler.py
@@ -1,8 +1,11 @@
-from skimage import io
+from fastapi import UploadFile
+from skimage.io import imread
+from io import BytesIO
 import numpy as np
 import urllib
 from keras.preprocessing.image import array_to_img, img_to_array
 
+from PIL import Image
 
 def preprocess(img: np.ndarray) -> np.ndarray:
     img = array_to_img(img, scale=False)
@@ -13,11 +16,24 @@ def preprocess(img: np.ndarray) -> np.ndarray:
 
 def handle_url(url: str) -> np.ndarray:
     try:
-        img_data = io.imread(url)
+        img_data = imread(url)
     except Exception:
         req = urllib.request.Request(url, headers={"User-Agent": "Magic Browser"})
         con = urllib.request.urlopen(req)
-        img_data = io.imread(con)
+        img_data = imread(con)
+    processed_img = preprocess(img_data)
+    img_array = np.array([processed_img])
+    return img_array
+
+
+def read_imagefile(file):
+    file_bytes = BytesIO(file)
+    image = Image.open(file_bytes)
+    return image
+
+
+def handle_file(file: UploadFile) -> np.ndarray:
+    img_data = read_imagefile(file)
     processed_img = preprocess(img_data)
     img_array = np.array([processed_img])
     return img_array
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, File
-from handler import handle_url
+from fastapi import FastAPI, UploadFile, File
+from handler import handle_file, handle_url
 from predict import predict_model
 
 app = FastAPI()
@@ -18,5 +18,7 @@ async def predict_url(url: str):
 
 
 @app.post("/predict_file")
-async def create_file(file: bytes = File(...)):
-    return {"file_size": len(file)}
+async def create_file(upload_file: UploadFile = File(...)):
+    model_input = handle_file(await upload_file.read())
+    model_output = predict_model(model_input)
+    return model_output
